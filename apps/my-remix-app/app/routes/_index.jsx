cb31308d75d81f269bdd56c54ddf85fd83fdb1bf
Pamela <pamvp@amazon.com>
2023-08-30 10:00:06 -0500

    push

--- apps/my-remix-app/app/routes/_index.jsx
+++ apps/my-remix-app/app/routes/_index.jsx
@@ -1,4 +1,4 @@
-//import { Button } from 'shared-ui';
+import { Button } from 'shared-ui';
 
 export const meta = () => {
   return [
@@ -7,43 +7,43 @@ export const meta = () => {
   ];
 };
 
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
 // export default function Index() {
 //   return (
-//     <div>
-//       <Button onClick={() => console.log('clicked')}>Click me</Button>
+//     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+//       <h1>Welcome to Remix</h1>
+//       <ul>
+//         <li>
+//           <a
+//             target="_blank"
+//             href="https://remix.run/tutorials/blog"
+//             rel="noreferrer"
+//           >
+//             15m Quickstart Blog Tutorial
+//           </a>
+//         </li>
+//         <li>
+//           <a
+//             target="_blank"
+//             href="https://remix.run/tutorials/jokes"
+//             rel="noreferrer"
+//           >
+//             Deep Dive Jokes App Tutorial
+//           </a>
+//         </li>
+//         <li>
+//           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+//             Remix Docs
+//           </a>
+//         </li>
+//       </ul>
 //     </div>
 //   );
 // }
+
+export default function Index() {
+  return (
+    <div>
+      <Button onClick={() => console.log('clicked')}>Click me</Button>
+    </div>
+  );
+}
